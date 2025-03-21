7aa538f7dc78cb33c32d0a632d24ad9b97d1c194
zhaoyi <492613075@qq.com>
2025-03-21 18:02:36 +0800

    vue项目初始化

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    1aaaaa
+    <SideMenu />   
+  </div>
+</template>
+<script>
+import SideMenu from './components/SideMenu.vue'
+export default {
+  components: {
+    SideMenu
+  }
+}
+</script>
+<style >
+  
+</style>
